69dac80abbde42c4e7dbe01750fd98834b5c5d6b
Alfredo A <almenaresalfredo@yahoo.com>
2019-04-03 09:54:06 -0400

    Update adder.js

--- adder.js
+++ adder.js
@@ -1,24 +1,25 @@
+//Promise with set timout
 const adder = async (previous,next)=> {
     return new Promise(resolve => setTimeout(
         resolve(previous+next)
         , 10));    
 } 
-
+//Async for each loop 
 const forEachAsync= async (nums)=>{
-    temp=0
+    temp=0                  //Accumulator
     
     await nums.forEach((n) => {    
         adder(temp,n)
-        .then((r)=>{temp+=r})
+        .then((r)=>{temp+=r})       //Accumulate results (addition)
     })
     return temp
 }
  
-//Function takes a list of todo
+//Asuync function takes a list of numbers
 const iterateNumbers = async (nums) => {  
    
     let total=await forEachAsync(nums)    
     console.log(total)
 }
 
-iterateNumbers([1,2,3,5,8,13,21])
+iterateNumbers([1,2,3,5,8,13,21])   //Asynchronously iterates, adding numbers, waiting 10 ms.
